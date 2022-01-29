8c661693fa8e2fc0f025bed57fc7582865131a41
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-01-29 20:15:48 -0300

    Update App.tsx

--- fills-calcuer/src/App.tsx
+++ fills-calcuer/src/App.tsx
@@ -1,13 +1,13 @@
 import { Background } from "./components/backgorund";
 import { GlobalStyle } from "./styles/styles";
-// import { CalcProvider } from "./context/ContextCalc";
+import { CalcProvider } from "./context/ContextCalc";
 
 function App() {
   return (
-    <>
+    <CalcProvider>
       <Background />
       <GlobalStyle />
-    </>
+    </CalcProvider>
   );
 }
 
