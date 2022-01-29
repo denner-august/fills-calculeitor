f34b405c86c672ab9609ad9e3fb814590cb728e8
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-01-29 20:15:42 -0300

    Update index.tsx

--- fills-calcuer/src/components/conteudo/carteira/index.tsx
+++ fills-calcuer/src/components/conteudo/carteira/index.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import { Calculadora } from "../calculadora";
+import { CalcContext } from "../../../context/ContextCalc";
 
 export function Carteira() {
   return (
