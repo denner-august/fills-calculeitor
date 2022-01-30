c9731d0e75e09a22822fa0c5b9e143a7377f3aee
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-01-30 19:02:21 -0300

    Update index.tsx

--- fills-calcuer/src/components/conteudo/calculadora/index.tsx
+++ fills-calcuer/src/components/conteudo/calculadora/index.tsx
@@ -4,15 +4,39 @@ interface inputProps {
   titulo: string;
   mensagemQuantidade: string;
   mensagemValor: string;
+  Quantidade: number;
+  setQuantidade: any;
+  Valor_atual: number;
+  muda_valor: any;
 }
 export function Calculadora(props: inputProps) {
-  const { titulo, mensagemQuantidade, mensagemValor } = props;
+  const {
+    titulo,
+    mensagemQuantidade,
+    mensagemValor,
+    Valor_atual,
+    muda_valor,
+    Quantidade,
+    setQuantidade,
+  } = props;
 
   return (
     <Container>
       <h2>{titulo}</h2>
-      <input type="number" placeholder={mensagemQuantidade} />
-      <input type="number" placeholder={mensagemValor} />
+
+      <input
+        type="number"
+        placeholder={mensagemQuantidade}
+        value={Quantidade}
+        onChange={(event) => setQuantidade(event.target.value)}
+      />
+
+      <input
+        type="currey"
+        placeholder={mensagemValor}
+        value={Valor_atual}
+        onChange={(event) => muda_valor(event.target.value)}
+      />
     </Container>
   );
 }
