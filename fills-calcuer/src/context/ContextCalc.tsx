300ebf28ec5a3e749974de5ff913efc52f78a2a6
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-01-30 19:02:13 -0300

    Update ContextCalc.tsx

--- fills-calcuer/src/context/ContextCalc.tsx
+++ fills-calcuer/src/context/ContextCalc.tsx
@@ -1,4 +1,5 @@
-import { createContext, useState, ReactNode, SetStateAction } from "react";
+import { createContext, useState, ReactNode } from "react";
+import Swal from "sweetalert2";
 
 interface CalcProps {
   Tenho_carteira: number;
@@ -11,6 +12,8 @@ interface CalcProps {
 
   Valor_mercado: number;
   setValor_mercado: (SetStateAction: number) => void;
+
+  Calcular: Function;
 }
 
 interface PropsProps {
@@ -20,10 +23,29 @@ interface PropsProps {
 export const CalcContext = createContext<CalcProps>({} as CalcProps);
 
 export function CalcProvider(props: { children: PropsProps }) {
-  const [Tenho_carteira, setTenho_carteira] = useState(0);
-  const [Valor_carteira, setValor_carteira] = useState(0);
-  const [Compra_mercado, setCompra_mercado] = useState(0);
-  const [Valor_mercado, setValor_mercado] = useState(0);
+  const [Tenho_carteira, setTenho_carteira] = useState(Number);
+  const [Valor_carteira, setValor_carteira] = useState(Number);
+
+  const [Compra_mercado, setCompra_mercado] = useState(Number);
+  const [Valor_mercado, setValor_mercado] = useState(Number);
+
+  function Calcular() {
+    if (
+      !Tenho_carteira ||
+      Number(Tenho_carteira) === 0 ||
+      !Valor_carteira ||
+      Number(Valor_carteira) === 0 ||
+      !Compra_mercado ||
+      Number(Compra_mercado) === 0 ||
+      !Valor_mercado ||
+      Number(Valor_mercado) === 0
+    ) {
+      return Swal.fire("Verifique os valores digitados");
+    }
+
+    // let total_quantidade = Number(Tenho_carteira) + Number(Compra_mercado);
+    // let total_valor = Number(Valor_carteira) + Number(Valor_mercado);
+  }
 
   return (
     <CalcContext.Provider
@@ -36,6 +58,8 @@ export function CalcProvider(props: { children: PropsProps }) {
         setCompra_mercado,
         Valor_mercado,
         setValor_mercado,
+
+        Calcular,
       }}
     >
       {props.children}
