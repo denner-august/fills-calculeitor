d0d7004049f1ac7403bbeaf9cb95e86921d81cfc
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-02-03 23:05:11 -0300

    Update App.tsx

--- fills-calcuer/src/App.tsx
+++ fills-calcuer/src/App.tsx
@@ -1,13 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Background } from "./components/backgorund";
 import { GlobalStyle } from "./styles/styles";
 import { CalcProvider } from "./context/ContextCalc";
+import { Resultado } from "./components/conteudo/Resultado";
 
 function App() {
   return (
-    <CalcProvider>
-      <Background />
-      <GlobalStyle />
-    </CalcProvider>
+    <Router>
+      <CalcProvider>
+        <Routes>
+          <Route path="/" element={<Background />} />
+          <Route path="/resultado" element={<Resultado />} />
+        </Routes>
+        <GlobalStyle />
+      </CalcProvider>
+    </Router>
   );
 }
 
