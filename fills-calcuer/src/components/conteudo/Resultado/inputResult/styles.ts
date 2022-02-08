4d84fbc54bb41abbd4162e1418b4b93ee5e23eae
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-02-07 21:04:53 -0300

    Create styles.ts

---
+++ fills-calcuer/src/components/conteudo/Resultado/inputResult/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+    height: 100%;
+    width: 100%;
+    border-radius: 1rem;   
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    flex-direction: column;
+    
+
+    p{
+        font-size: 1.5rem;
+    }
+
+`
+
+export const Input = styled.div`
+    height: 4rem;
+    width: 80%;
+    background-color:#DB5375;
+    margin: 0.5rem;
+    border-radius: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 1.5rem;
+`
