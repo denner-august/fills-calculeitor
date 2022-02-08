bdb877d25f61d642f5d43ab60d96efb7733d41c0
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-02-07 21:05:03 -0300

    Update index.tsx

--- fills-calcuer/src/components/conteudo/Resultado/index.tsx
+++ fills-calcuer/src/components/conteudo/Resultado/index.tsx
@@ -1,15 +1,32 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { CalcContext } from "../../../context/ContextCalc";
+import { InputResult } from "./inputResult";
 import { Container, FinalResultado } from "./styles";
 
 export function Resultado() {
   const { total_quantidade, total_valor } = useContext(CalcContext);
   const navigate = useNavigate();
+
   return (
     <Container>
       <FinalResultado>
-        <h2>Resultado</h2>
+        <InputResult
+          Valores={total_quantidade}
+          titulo="Nova Quantidade Total"
+        />
+        <InputResult Valores={total_valor} titulo="Seu Novo Valor Total" />
+        <InputResult titulo="titulo" />
+        <InputResult titulo="titulo" />
+
+        <button onClick={() => navigate("/")}>Voltar</button>
+      </FinalResultado>
+    </Container>
+  );
+}
+
+/* 
+  <h2>Resultado</h2>
         <p>
           você tera um novo total de{" "}
           {total_quantidade ? total_quantidade : null} ações
@@ -24,7 +41,6 @@ export function Resultado() {
         </p>
 
         <button onClick={() => navigate("/")}>Voltar</button>
-      </FinalResultado>
-    </Container>
-  );
-}
+
+
+*/
