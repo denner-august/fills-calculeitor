c200a9461941796803045458fa4d9c3a16f58854
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-03-03 23:16:34 -0300

    Update styles.ts

--- fills-calcuer/src/components/conteudo/Resultado/inputResult/styles.ts
+++ fills-calcuer/src/components/conteudo/Resultado/inputResult/styles.ts
@@ -2,13 +2,10 @@ import styled from 'styled-components'
 
 
 export const Container = styled.div`
-    height: 100%;
     width: 100%;
     border-radius: 1rem;   
     display: flex;
-    justify-content: center;
     align-items: center;
-    text-align: center;
     flex-direction: column;
     
 
@@ -19,7 +16,7 @@ export const Container = styled.div`
 `
 
 export const Input = styled.div`
-    height: 4rem;
+    height: 4rem;  
     width: 80%;
     background-color:#DB5375;
     margin: 0.5rem;
