36d6a87e8281608036bda41f0383e6e9fbe04717
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-03-03 23:16:46 -0300

    Update styles.ts

--- fills-calcuer/src/components/background-conteudo/styles.ts
+++ fills-calcuer/src/components/background-conteudo/styles.ts
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
- width: 40vw;
+width: 40vw;
+ min-width: 35rem;
  height: 50rem;
  background-color: #f7f7f7;
  border-radius: 10px;
@@ -11,6 +12,7 @@ display: flex;
 padding: 20px;
 flex-direction: column;
 align-items: center;
+
 
 h1,h2,p{
     color: black;
