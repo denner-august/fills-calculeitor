2e2a80f005ce5124edd1601c956ecee13d7b2f4b
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-13 19:25:11 -0300

    configuração inicial

---
+++ next-flls/src/components/Controller.tsx
@@ -0,0 +1,9 @@
+import Layout from "./Layout";
+
+export function Controller() {
+  return (
+    <>
+      <Layout />
+    </>
+  );
+}
---
+++ next-flls/src/components/Layout/index.tsx
@@ -0,0 +1,3 @@
+export default function Layout() {
+  return <h2>layout</h2>;
+}
