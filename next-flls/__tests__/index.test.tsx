1c14f508ced4ea7e0b621d5dfa431b360f8b11e5
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-14 13:31:17 -0300

    Update index.test.tsx

--- next-flls/__tests__/index.test.tsx
+++ next-flls/__tests__/index.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Layout } from "../src/components/Layout";
 
-//  screen.logTestingPlaygroundURL();
+const playground = () => screen.logTestingPlaygroundURL();
 
 const Component_Layout = <Layout />;
 
@@ -20,4 +20,45 @@ test("verifica titulo da pagina", () => {
   const GetTitle = screen.getByText("Preço Médio");
 
   expect(GetRoleTitle && GetTitle).toBeInTheDocument();
+});
+
+describe("Verificando placeholder e inputs", () => {
+  test("verificando titulo da div e valor", () => {
+    render(Component_Layout);
+
+    const TitleCaritera = screen.getByRole("heading", {
+      name: /carteira/i,
+    });
+
+    const TitleCompra = screen.getByRole("heading", {
+      name: /compra/i,
+    });
+
+    expect(TitleCaritera && TitleCompra).toBeInTheDocument();
+  });
+
+  test("testando inputs da Carteira", () => {
+    render(Component_Layout);
+
+    const CarteiraPlaceholder = screen.getByPlaceholderText(
+      "Quantas ações Você tem?"
+    );
+    const ValorCarteiraPlaceholder =
+      screen.getByPlaceholderText("Qual o preço?");
+
+    expect(CarteiraPlaceholder && ValorCarteiraPlaceholder).toBeInTheDocument();
+  });
+
+  test("testando inputs da Compra", () => {
+    render(Component_Layout);
+
+    const CompraPlaceholder = screen.getByPlaceholderText(
+      "Quantas ações você vai compra?"
+    );
+    const ValorCompraPlaceholder = screen.getByPlaceholderText(
+      "Qual o valor da ação?"
+    );
+
+    expect(CompraPlaceholder && ValorCompraPlaceholder).toBeInTheDocument();
+  });
 });
