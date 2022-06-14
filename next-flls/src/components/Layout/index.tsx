a86393ecd9a7f978844df75b1ed6cac1840678d9
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-14 13:31:51 -0300

    Update index.tsx

--- next-flls/src/components/Layout/index.tsx
+++ next-flls/src/components/Layout/index.tsx
@@ -1,9 +1,21 @@
 import styles from "./styles.module.scss";
+import { Valores } from "../inputValores/index";
 
 export function Layout() {
   return (
     <div role="Layout" className={styles.Container}>
       <h1 role="title">Preço Médio</h1>
+      <Valores
+        Titulo="Carteira"
+        CarteiraPlaceholder="Quantas ações Você tem?"
+        ValorPlaceholder="Qual o preço?"
+      />
+
+      <Valores
+        Titulo="Compra"
+        CarteiraPlaceholder="Quantas ações você vai compra?"
+        ValorPlaceholder="Qual o valor da ação?"
+      />
     </div>
   );
 }
