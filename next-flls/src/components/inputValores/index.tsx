39ff9e35b38f23f615b66386355fdf7864adfef8
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-20 16:01:28 -0300

    Update index.tsx

--- next-flls/src/components/inputValores/index.tsx
+++ next-flls/src/components/inputValores/index.tsx
@@ -4,18 +4,75 @@ interface ValoresProps {
   Titulo?: string;
   CarteiraPlaceholder?: string;
   ValorPlaceholder?: string;
+
+  açõesQuantidade: number;
+  açõesPreço: number;
+
+  dispatch: (objeto: {
+    userAçõesQuantidade: number;
+    userPreçoAções: number;
+    QuantiCompraAções: number;
+    CompraPreçoAções: number;
+  }) => void;
+
+  usuario?: Boolean;
 }
 
 export function Valores({
   Titulo = "Carteira",
   CarteiraPlaceholder = "Digite o numero de ações",
   ValorPlaceholder = "Digite o valor",
+  açõesQuantidade,
+  açõesPreço,
+
+  dispatch,
+  usuario,
 }: ValoresProps) {
   return (
     <div className={styles.Container}>
       <h2>{Titulo}</h2>
-      <input placeholder={CarteiraPlaceholder} type="number" />
-      <input placeholder={ValorPlaceholder} type="number" />
+
+      <input
+        value={açõesQuantidade === 0 ? "" : açõesQuantidade}
+        placeholder={CarteiraPlaceholder}
+        type="number"
+        onChange={(event) =>
+          usuario
+            ? dispatch({
+                userAçõesQuantidade: Number(event.target.value),
+                userPreçoAções: açõesPreço,
+                QuantiCompraAções: 0,
+                CompraPreçoAções: 0,
+              })
+            : dispatch({
+                userAçõesQuantidade: 0,
+                userPreçoAções: 0,
+                QuantiCompraAções: Number(event.target.value),
+                CompraPreçoAções: açõesPreço,
+              })
+        }
+      />
+
+      <input
+        value={açõesPreço === 0 ? "" : açõesPreço}
+        placeholder={ValorPlaceholder}
+        type="number"
+        onChange={(event) =>
+          usuario
+            ? dispatch({
+                userAçõesQuantidade: açõesQuantidade,
+                userPreçoAções: Number(event.target.value),
+                QuantiCompraAções: 0,
+                CompraPreçoAções: 0,
+              })
+            : dispatch({
+                userAçõesQuantidade: 0,
+                userPreçoAções: 0,
+                QuantiCompraAções: açõesQuantidade,
+                CompraPreçoAções: Number(event.target.value),
+              })
+        }
+      />
     </div>
   );
 }
