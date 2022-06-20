ca0e8d008969f2c9716ef7bb7ff7996e8cf0f566
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-20 16:01:20 -0300

    Update index.tsx

--- next-flls/src/components/Layout/index.tsx
+++ next-flls/src/components/Layout/index.tsx
@@ -1,24 +1,51 @@
 import styles from "./styles.module.scss";
 import { Valores } from "../inputValores/index";
 import { ButtonCalcular } from "../buttonCalcular";
+import { useContext, useState } from "react";
+import { FllsContext } from "../../../context/Context";
+
+import { Result } from "../Result/index";
 
 export function Layout() {
+  const [mostraResult, setMostraResult] = useState(false); // criar variavel no context para controlar qual component redenrizar
+
+  const {
+    usuario: { userAçõesQuantidade, userPreçoAções },
+    ações: { CompraPreçoAções, QuantiCompraAções },
+    setAções,
+    setUserAçõesQuantidade,
+  } = useContext(FllsContext);
+
   return (
-    <div role="Layout" className={styles.Container}>
-      <h1 role="title">Preço Médio</h1>
-      <Valores
-        Titulo="Carteira"
-        CarteiraPlaceholder="Quantas ações Você tem?"
-        ValorPlaceholder="Qual o preço?"
-      />
+    <>
+      {mostraResult ? (
+        <Result />
+      ) : (
+        <div role="Layout" className={styles.Container}>
+          <h1 role="title">Preço Médio</h1>
 
-      <Valores
-        Titulo="Compra"
-        CarteiraPlaceholder="Quantas ações você vai compra?"
-        ValorPlaceholder="Qual o valor da ação?"
-      />
+          <Valores
+            açõesQuantidade={userAçõesQuantidade}
+            açõesPreço={userPreçoAções}
+            Titulo="Carteira"
+            CarteiraPlaceholder="Quantas ações Você tem?"
+            ValorPlaceholder="Qual o preço?"
+            dispatch={setUserAçõesQuantidade}
+            usuario
+          />
 
-      <ButtonCalcular />
-    </div>
+          <Valores
+            açõesPreço={CompraPreçoAções}
+            açõesQuantidade={QuantiCompraAções}
+            Titulo="Compra"
+            CarteiraPlaceholder="Quantas ações você vai compra?"
+            ValorPlaceholder="Qual o valor da ação?"
+            dispatch={setAções}
+          />
+
+          <ButtonCalcular />
+        </div>
+      )}
+    </>
   );
 }
