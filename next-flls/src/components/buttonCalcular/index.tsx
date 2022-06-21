f018681432f797a12a6b4f96f39859dea7a77cb9
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-21 13:31:22 -0300

    Update index.tsx

--- next-flls/src/components/buttonCalcular/index.tsx
+++ next-flls/src/components/buttonCalcular/index.tsx
@@ -1,5 +1,21 @@
 import styles from "./styles.module.scss";
+import { useContext } from "react";
+import { FllsContext } from "context/Context";
 
-export function ButtonCalcular() {
-  return <button className={styles.Button}>Calcular</button>;
+interface ButtonProps {
+  nomeButton: string;
+  funcaoChamada: any;
+}
+
+export function ButtonCalcular({
+  funcaoChamada,
+  nomeButton = "teste",
+}: ButtonProps) {
+  // const { setMostraResult } = useContext(FllsContext);
+
+  return (
+    <button onClick={funcaoChamada} className={styles.Button}>
+      {nomeButton}
+    </button>
+  );
 }
