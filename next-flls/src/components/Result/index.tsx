998767207a60527354205327c3bc4cb0ac0c2658
Denner augusto De Paula Bernardes <41222616+denner-august@users.noreply.github.com>
2022-06-21 13:30:56 -0300

    Update index.tsx

--- next-flls/src/components/Result/index.tsx
+++ next-flls/src/components/Result/index.tsx
@@ -1,3 +1,24 @@
+import { ButtonCalcular } from "../buttonCalcular";
+import styles from "./styles.module.scss";
+
+import stylesContainerInput from "../inputValores/styles.module.scss";
+import { useContext } from "react";
+import { FllsContext } from "context/Context";
 export function Result() {
-  return <h1>resultado</h1>;
+  const { setMostraResult } = useContext(FllsContext);
+
+  return (
+    <div className={stylesContainerInput.Container}>
+      <h2>Nova Quantidade Total</h2>
+      <input type="text" />
+      <h2>Seu Novo Valor Total</h2>
+      <input type="text" />
+      <h2>Novo preço médio</h2>
+      <input type="text" />
+      <ButtonCalcular
+        nomeButton="Voltar"
+        funcaoChamada={() => setMostraResult(false)}
+      />
+    </div>
+  );
 }
