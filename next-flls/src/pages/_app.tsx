4e0577d9a4793d6f4dd14e73974f6751658b6f91
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-04-07 20:04:01 -0300

    Update _app.tsx

--- next-flls/src/pages/_app.tsx
+++ next-flls/src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { FllsContextProvider } from "context/Context";
+import { ToastContainer } from "react-toastify";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <FllsContextProvider>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
       <Component {...pageProps} />
     </FllsContextProvider>
   );
